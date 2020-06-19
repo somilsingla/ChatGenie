a6fff2ab17c731c2619b5ac67ed0ff8ff80394ec
somilsingla <somilsingla12@gmail.com>
2020-06-19 19:45:32 +0530

    Latest

--- app/src/main/java/com/example/chatgenie/MainActivity.java
+++ app/src/main/java/com/example/chatgenie/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+// Main Activity containing the button to access the chat-bot
+
 public class MainActivity extends AppCompatActivity {
     ImageButton button;
 
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // OnCLickListener to start the chat-bot
 
         button = (ImageButton) findViewById(R.id.botButton);
         button.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/chatgenie/messagelist.java
+++ app/src/main/java/com/example/chatgenie/messagelist.java
@@ -29,7 +29,12 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+// Chat-bot activity
+
 public class messagelist extends AppCompatActivity {
+
+    // Making the variables for the chat items and the recycler view and it's adapter
+
     private RecyclerView mMessageRecycler;
     private MessageListAdapter mMessageAdapter;
 
@@ -47,6 +52,8 @@ public class messagelist extends AppCompatActivity {
         setContentView(R.layout.activity_messagelist);
         chat = new Chat();
 
+        // Hard coded greeting by the chat-bot is displayed in the view
+
         chat.setMessage("Hello! How may I help you?");
         chat.setSender("bot");
         chatList.add(chat);
@@ -56,6 +63,7 @@ public class messagelist extends AppCompatActivity {
         mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
         mMessageRecycler.setAdapter(mMessageAdapter);
 
+        // User sending query
 
         imageButton = (ImageButton) findViewById(R.id.button_chatbox_send);
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -71,21 +79,29 @@ public class messagelist extends AppCompatActivity {
                 mMessageRecycler.setAdapter(mMessageAdapter);
                 EditText editText = (EditText) findViewById(R.id.edittext_chatbox);
                 editText.setText("");
+
+                // Putting the user query as a JSON object
                 try {
-                    query.put("message",temp1.getMessage().toString());
+                    query.put("message", temp1.getMessage().toString());
                 } catch (JSONException e) {
                     Log.d("OKHTTP3","JSON Exception");
                     e.printStackTrace();
                 }
 
-                OkHttpClient okHttpClient = new OkHttpClient();
-                String url = "http://dummy.restapiexample.com/api/v1/employee/1";
+                // Setting up a HTTP Client to post request to backend
 
+                OkHttpClient okHttpClient = new OkHttpClient();
+                String url = "http://104.215.195.198/api/messages";
+
+                // Posting request to the backend server
                 MediaType JSON = MediaType.parse("application/json");
-                RequestBody requestBody = RequestBody.create(JSON, query.toString() );
+                RequestBody requestBody = RequestBody.create(query.toString(),JSON);
                 Request request = new Request.Builder()
                                     .url(url)
-                                    .get().build();
+                                    .addHeader("Content-Type","application/json")
+                                    .addHeader("Accept","/")
+                                    .addHeader("Host","104.215.195.198")
+                                    .post(requestBody).build();
 
                 okHttpClient.newCall(request).enqueue(new Callback() {
                     @Override
@@ -94,38 +110,57 @@ public class messagelist extends AppCompatActivity {
                     }
 
                     @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        try {
-                            if(response.isSuccessful())
-                            reply = new JSONObject(response.body().string());
-                            //botmessage = reply.getString("message");
-                            botmessage = reply.getString("status");
+                    public void onResponse(Call call, final Response response) throws IOException {
+                        messagelist.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                                    // Receiving the response from the server and displaying
+
+                                    if(response.isSuccessful()) {
+
+                                        assert response.body() != null;
+                                        reply = new JSONObject(response.body().string());
+                                        botmessage = reply.getString("message");
+
+                                    }
+                                    else {
+                                        Log.d("Fail","Response not succesful");
+                                        botmessage = "Response is not succesful";
+                                    }
+
+                                    Chat temp2 = new Chat();
+                                    temp2.setMessage(botmessage);
+                                    temp2.setSender("bot");
+                                    chatList.add(temp2);
+                                    mMessageAdapter = new MessageListAdapter(getApplicationContext(), chatList);
+                                    mMessageRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                                    mMessageRecycler.setAdapter(mMessageAdapter);
+
+
+                                }catch (JSONException | IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        });
+
                     }
                 });
 
 
-                Chat temp2 = new Chat();
-                temp2.setMessage(botmessage);
-                temp2.setSender("bot");
-                chatList.add(temp2);
-                mMessageAdapter = new MessageListAdapter(getApplicationContext(), chatList);
-                mMessageRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                mMessageRecycler.setAdapter(mMessageAdapter);
 
             }
         });
 
     }
 
+    //Getting the query entered by the user
+
     public void usermessage() {
         EditText edText1 = (EditText) findViewById(R.id.edittext_chatbox);
         message = edText1.getText().toString();
     }
-
-
 
 }
